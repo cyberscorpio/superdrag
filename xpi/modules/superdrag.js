a6687c722d17b9279a6f0dc074eaf1edf4466652
cyberscorpio <cyberscorpio@gmail.com>
2013-11-24 00:18:07 +0800

    go go go

--- xpi/modules/superdrag.js
+++ xpi/modules/superdrag.js
@@ -12,7 +12,9 @@ Cu.import("resource://gre/modules/Services.jsm");
 var SuperDrag = new function() {
 	const PANELID = 'superdrag-panel';
 	let that = this;
-	let defHandlers = {
+	let gUrlPattern = /^https?:\/\/w{0,3}\w*?\.(\w*?\.)?\w{2,3}\S*|www\.(\w*?\.)?\w*?\.\w{2,3}\S*|(\w*?\.)?\w*?\.\w{2,3}[\/\?]\S*$/;
+	let gStr = Cc["@mozilla.org/intl/stringbundle;1"].getService(Ci.nsIStringBundleService).createBundle("chrome://superdrag/locale/strings.properties");
+	let gDefHandlers = {
 		'dragstart': function(evt) {
 			/*
 			var wm = getMainWindow();
@@ -61,7 +63,7 @@ var SuperDrag = new function() {
 					let tb = wm.document.getElementById('content');
 					tb.addTab(data);
 				} else if (key == 'text' || key == 'selection') {
-					let engine = engines.currentEngine;
+					let engine = gEngines.currentEngine;
 					let submission = engine.getSubmission(data);
 					let url = submission.uri.spec;
 
@@ -73,10 +75,10 @@ var SuperDrag = new function() {
 		},
 		'dragend': function(evt) {
 			afterDrag();
-			log('dropped');
+			log('drag end @' + (new Date()).toString());
 		},
 	};
-	let panelHandlers = {
+	let gPanelHandlers = {
 		'dragenter': function(evt) {
 			let t = evt.target;
 			if (t.classList.contains('superdrag-target')) {
@@ -85,6 +87,7 @@ var SuperDrag = new function() {
 			} else {
 				updateActionString(getActionString(null));
 			}
+			gSearchEngineMenuManager.onEnter(evt);
 		},
 		'dragover': function(evt) {
 			evt.preventDefault();
@@ -94,6 +97,7 @@ var SuperDrag = new function() {
 			if (t.classList.contains('superdrag-target')) {
 				t.classList.remove('hover');
 			}
+			gSearchEngineMenuManager.onLeave(evt);
 		},
 		'drop': function(evt) {
 			targetOnDrop(evt);
@@ -101,7 +105,7 @@ var SuperDrag = new function() {
 			evt.stopPropagation();
 		},
 	};
-	let engines = Cc['@mozilla.org/browser/search-service;1'].getService(Ci.nsIBrowserSearchService);
+	let gEngines = Cc['@mozilla.org/browser/search-service;1'].getService(Ci.nsIBrowserSearchService);
 
 
 	// -------------------------------------------------------------------------------- 
@@ -158,11 +162,11 @@ var SuperDrag = new function() {
 			let tb = doc.getElementById('content'); // TabBrowser
 			if (tb && tb.mPanelContainer) {
 				let pc = tb.mPanelContainer;
-				for (let k in defHandlers) {
+				for (let k in gDefHandlers) {
 					if (uninstall) {
-						pc.removeEventListener(k, defHandlers[k], false);
+						pc.removeEventListener(k, gDefHandlers[k], false);
 					} else {
-						pc.addEventListener(k, defHandlers[k], false);
+						pc.addEventListener(k, gDefHandlers[k], false);
 					}
 				}
 			}
@@ -173,8 +177,8 @@ var SuperDrag = new function() {
 				let panel = doc.getElementById(PANELID);
 				// remove the event listeners.
 				if (panel) {
-					for (let k in panelHandlers) {
-						panel.removeEventListener(k, panelHandlers[k], false);
+					for (let k in gPanelHandlers) {
+						panel.removeEventListener(k, gPanelHandlers[k], false);
 					}
 
 					// remove the panel
@@ -208,8 +212,8 @@ var SuperDrag = new function() {
 					observe: function(sub, topic, data) {
 						if (topic == 'xul-overlay-merged') {
 							let panel = doc.getElementById(PANELID);
-							for (let k in panelHandlers) {
-								panel.addEventListener(k, panelHandlers[k], false);
+							for (let k in gPanelHandlers) {
+								panel.addEventListener(k, gPanelHandlers[k], false);
 							}
 						}
 					}
@@ -227,6 +231,7 @@ var SuperDrag = new function() {
 	let gDataset = null;
 	let gPos = null;
 	let gPanel = null;
+	let gSearchEngineMenu = null;
 
 	// -------------------------------------------------------------------------------- 
 	// local functions
@@ -280,33 +285,28 @@ var SuperDrag = new function() {
 			}
 		}
 
-		if (d['primaryKey'] === undefined) {
-			return null;
-		}
-
-//		dump(evt);
-//		log(evt.target == evt.originalTarget);
-//		log(evt.target.tagName);
-//		log(evt.explicitOriginalTarget.tagName);
-//		log(evt.currentTarget.tagName);
-
-//		log(' types:');
-//		for (let i = 0; i < dt.types.length; ++ i) {
-//			log(' - ' + dt.types.item(i) + ': ' + dt.getData(dt.types.item(i)));
-//		}
-//		log(evt.target.tagName);
-//		dump(dt);
-
-
 		// selection(s)
 		let sel = evt.target.ownerDocument.defaultView.getSelection();
 		sel = sel.toString();
 		if (sel != '') {
 			d['selection'] = sel;
 			d['primaryKey'] = 'selection';
+
 		}
 
-//		dump(d);
+		let text = d['selection'] || d['text'];
+		if (text) {
+			// TODO: the pattern should work for xxx.com/.net/.info
+			if (gUrlPattern.test(text)) {
+				d['link'] = text;
+				d['primaryKey'] = 'link';
+			}
+		}
+
+
+		if (d['primaryKey'] === undefined) {
+			return null;
+		}
 
 		return d;
 	}
@@ -315,6 +315,7 @@ var SuperDrag = new function() {
 		gDataset = null;
 		gPos = null;
 		try {
+			gSearchEngineMenuManager.onEnd();
 			if (gPanel) {
 				let hovers = gPanel.getElementsByClassName('hover');
 				while(hovers.length > 0) {
@@ -399,12 +400,13 @@ var SuperDrag = new function() {
 			['link', 'text', 'image'].forEach(function(cat) {
 				let section = doc.getElementById('superdrag-' + cat);
 				let desc = doc.getElementById('superdrag-' + cat + '-desc');
-				if (gDataset[cat] === undefined) {
+				let data = (cat == 'text' ? (gDataset['selection'] || gDataset['text']) : gDataset[cat]);
+				if (data === undefined) {
 					section.classList.add('hide');
 					desc.setAttribute('value', null);
 				} else {
 					section.classList.remove('hide');
-					desc.setAttribute('value', gDataset[cat]);
+					desc.setAttribute('value', data);
 				}
 			});
 
@@ -416,13 +418,130 @@ var SuperDrag = new function() {
 			// gPanel.openPopupAtScreen(wm.screenX + rc.right - gPanel.scrollWidth - 20, wm.screenY + rc.top);
 			gPanel.openPopupAtScreen(sX, sY);
 
-		//	rc = gPanel.getBoundingClientRect();
-		//	log('width: ' + (rc.right - rc.left));
+			// DON'T remove them because:
+			//  below code is used to report the width of the panel,
+			//  which is useful for the CSS file.
+			// rc = gPanel.getBoundingClientRect();
+			// log('width: ' + (rc.right - rc.left));
 
 			// 3. set the action name
 			updateActionString(getActionString(null));
 		}
 	}
+
+	let gSearchEngineMenuManager = new function() {
+		let id = 'superdrag-text-search';
+		let menuid = 'superdrag-text-search-engines';
+		let popup = null;
+		let tmShow = null;
+		let tmHide = null;
+
+		this.onEnter = function(evt) {
+			let el = evt.target;
+			let doc = el.ownerDocument;
+			let win = doc.defaultView;
+			let p = doc.getElementById(id);
+			let menu = doc.getElementById(menuid);
+
+			if (!p.contains(el)) {
+				return;
+			}
+
+			if (popup == null && tmShow == null) {
+				tmShow = win.setTimeout(showMenu, 500);
+			}
+
+			if (popup) {
+				if (el.tagName == 'menuitem') {
+					el.setAttribute('_moz-menuactive', true);
+					el.setAttribute('menuactive', true);
+
+					updateActionString(getString('sdSearchWith').replace('%engine%', el.getAttribute('value')));
+				}
+			}
+
+			win.setTimeout(function() {
+				if (tmHide) {
+					win.clearTimeout(tmHide);
+					tmHide = null;
+				}
+			}, 0);
+		};
+
+		this.onLeave = function(evt) {
+			let el = evt.target;
+			let doc = el.ownerDocument;
+			let win = doc.defaultView;
+			let p = doc.getElementById(id);
+			let menu = doc.getElementById(menuid);
+
+			if (p.contains(el) && el.tagName == 'menuitem') {
+				el.removeAttribute('_moz-menuactive');
+				el.removeAttribute('menuactive');
+			}
+
+
+			if (evt.relatedTarget && p.contains(evt.relatedTarget)) {
+				return;
+			}
+
+			if (popup && tmHide == null) {
+				tmHide = win.setTimeout(hideMenu, 500);
+			}
+		};
+
+		this.onEnd = function() {
+			if (popup) {
+				popup.hidePopup();
+				popup = null;
+			}
+
+			if (gPanel) {
+				let doc = gPanel.ownerDocument;
+				let win = doc.defaultView;
+				tmShow && win.clearTimeout(tmShow);
+				tmHide && win.clearTimeout(tmHide);
+			}
+
+			tmShow = tmHide = null;
+		}
+
+		function showMenu() {
+			tmShow = null;
+			if (gPanel && popup == null) {
+				let doc = gPanel.ownerDocument;
+				let menu = doc.getElementById('superdrag-text-search-engines');
+				while(menu.children.length > 0) {
+					menu.removeChild(menu.firstChild);
+				}
+				// populate the engines
+				let engines = gEngines.getVisibleEngines();
+				for (let i = 0; i < engines.length; ++ i) {
+					let engine = engines[i];
+					let m = doc.createElement("menuitem");
+					m.setAttribute('label', engine.name);
+					if (engine.iconURI && engine.iconURI.spec) {
+						m.setAttribute('image', engine.iconURI.spec);
+					}
+					m.setAttribute('class', "menuitem-iconic bookmark-item");
+					m.setAttribute('value', engine.name);
+					m.setAttribute('s-index', i);
+					menu.appendChild(m);
+				}
+				menu.openPopup(menu.parentNode, 'after_end', 0, 0);
+				popup = menu;
+			}
+		}
+	
+		function hideMenu() {
+			tmHide = null;
+			if (popup) {
+				popup.hidePopup();
+				popup = null;
+			}
+		}
+
+	};
 
 	function getActionString(id) {
 		if (id === null) {
@@ -456,6 +575,17 @@ var SuperDrag = new function() {
 				label.setAttribute('value', s);
 			}
 		}
+	}
+
+	function getString(k) {
+		let s = '';
+		try {
+			s = gStr.GetStringFromName(k);
+		} catch (e) {
+			Cu.reportError(e);
+		}
+
+		return s;
 	}
 
 	function dump(o, arg) {
